024e85bae23dfab33aa7cadd57eeb8cc78d8bbfd
Greg Adams <mowgli.gregory1@gmail.com>
2022-11-27 20:28:49 -0500

    adding M function, havent tested it yet

--- Lab3.c
+++ Lab3.c
@@ -120,7 +120,7 @@ int main(int argc, char* argv[])
 // P function
 if(strcmp(argv[1],"P")==0)
 {
-    char fileName[8];
+    char fileName[8]; // make 8 or 9? 
       for (i=0; i<512; i=i+16) {
         if (dir[i]==0) break;
         for (j=0; j<8; j++) {
@@ -168,6 +168,144 @@ if(strcmp(argv[1],"P")==0)
 
 
 }
+
+/// M function
+
+
+if(strcmp(argv[1],"M")==0) // argv[2] is the file name
+{
+    char contents[512]; // max size is 1 sector 
+    int startPoint; // (i+9) points to where the sector will begin at.
+    char fileName[10];
+    char dirSearch[9]; // char temp[9];
+    dirSearch[8] = '\0';
+    int boolFlag = 0;
+    //printf("Enter the text for your file now: \n");
+    //fgets(contents, 512, stdin);
+    //
+    for (i = 0; i < 8;++i)
+    {
+        fileName[i] = argv[2][i];
+    }
+
+    int strLength = strlen(argv[2]); 
+    if (strLength < 8)
+    {
+        for (i = 0; i < 8 - strLength; ++i )
+        { 
+
+            fileName[7-i] = '0'; 
+        } 
+    }
+    fileName[9] = '\0'; // need null terminator ? maybe? we will be safe
+    fileName[8] = 't';
+
+    for (i=0; i<512; i=i+16) 
+    {
+        for (j=0; j<8; j++) {
+            if (dir[i+j]==0) dirSearch[j] ='\0'; else dirSearch[j]=dir[i+j];
+        }
+    }
+    if(strcmp(dirSearch, fileName)==0) // BAD (if we find 2 file with same name)
+    {
+            printf("duplicate or invalid file name \n"); // exit
+            return -1; // end program maybe use break?
+    }
+    else 
+    {
+        printf("Enter the text for your file now: \n");
+        fgets(contents, 512, stdin);
+        for(i=0; i<512; i=i+16) 
+        {
+            if(dir[i]==0) // look for free space in directory
+                {
+                    printf("we have found a free space! \n");
+                    for(j=0; j<9; j++) 
+                    {
+                        dir[i+j] == fileName[j];
+                    }
+                    for(int j=0; j<512; j++)
+                    {
+                        if(map[j]==0)
+                        {
+                            startPoint = j;
+                            map[j] = 255; // set n
+                            boolFlag = 1;  //trigger bool 
+                        }
+                    }
+                    dir[i+9] = startPoint;
+                    dir[i+10] = 1;
+
+                    fseek(floppy,512*dir[i+9],SEEK_SET);
+                    for(i=0; i<512*dir[i+10]; i++) 
+                    {
+                       fprintf(floppy+i, "%c", contents[i]);
+                    }
+                    return 0;
+                }
+
+            if (boolFlag)
+                {
+                    break;
+                } 
+            }
+            
+            
+    }
+
+    if (boolFlag = 0)
+    {
+        printf("No disks space :(( \n");
+    }
+}
+
+
+
+
+//load the disk map from sector 256
+	
+	fseek(floppy,512*256,SEEK_SET);
+	for(i=0; i<512; i++)
+		map[i]=fgetc(floppy);
+
+	//load the directory from sector 257
+	
+	fseek(floppy,512*257,SEEK_SET);
+	for (i=0; i<512; i++)
+		dir[i]=fgetc(floppy);
+
+    //print disk map
+	printf("Disk usage map:\n");
+	printf("      0 1 2 3 4 5 6 7 8 9 A B C D E F\n");
+	printf("     --------------------------------\n");
+	for (i=0; i<16; i++) {
+		switch(i) {
+			case 15: printf("0xF_ "); break;
+			case 14: printf("0xE_ "); break;
+			case 13: printf("0xD_ "); break;
+			case 12: printf("0xC_ "); break;
+			case 11: printf("0xB_ "); break;
+			case 10: printf("0xA_ "); break;
+			default: printf("0x%d_ ", i); break;
+		}
+		for (j=0; j<16; j++) {
+			if (map[16*i+j]==-1) printf(" X"); else printf(" .");
+		}
+		printf("\n");
+	}
+
+    // print directory
+	printf("\nDisk directory:\n");
+	printf("Name    Type Start Length\n");
+    for (i=0; i<512; i=i+16) {
+		if (dir[i]==0) break;
+		for (j=0; j<8; j++) {
+			if (dir[i+j]==0) printf(" "); else printf("%c",dir[i+j]);
+		}
+		if ((dir[i+8]=='t') || (dir[i+8]=='T')) printf("text"); else printf("exec");
+		printf(" %5d %6d bytes\n", dir[i+9], 512*dir[i+10]);
+	}
+
 
 
 
